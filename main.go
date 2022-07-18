8165f87fc795250c49a4ce502a8a67098d8b6591
Patrick Putman <putman.patrick@gmail.com>
2022-07-18 11:59:22 -0500

    db <3 jeez

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func main() {
 
         log.Println("Using nick ", nick)
 
-        channel := "#holybible"
+        channel := "#gulag"
 
         randLength := 30
 
@@ -84,7 +84,7 @@ func main() {
                                 go func() {
                                         for i := 0; i < 50; i++ {
                                                 randString := StringWithCharset(randLength, charSet)
-                                                message := "Debaser fucks jesus!!!" + randString
+                                                message := "Debaser owns you!!!" + randString
                                                 time.Sleep(time.Second)
                                                 err := c.WriteMessage(&irc.Message{
                                                         Command: "PRIVMSG",
